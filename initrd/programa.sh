5d7ad81eae11937582a68058aaa9c4da2898289e
ArtEze <passymas@gmail.com>
2021-02-20 19:37:48 +0000

    Create programa.sh

---
+++ initrd/programa.sh
@@ -0,0 +1,20 @@
+#!/bin/ash
+
+esperar_bash 6 ""
+
+montar_todo
+. "$carpeta_sd_funciones/p.funciones.sh"
+
+copiar_inipup
+
+desmontar_todo
+
+inicio="./inicio.sh"
+if [[ -f $inicio ]];then
+	. "$inicio"
+else
+	echo "El inicio ha fallado. No existe inicio."
+	echo "Presione una tecla para continuar."
+	read -t 5 -n 1 variable
+	ash
+fi
